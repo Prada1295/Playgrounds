16bd4eb44fd842f9018ddcb10835e2e7e1baec2c
Andres Prada <prada1295@gmail.com>
2025-05-27 21:51:38 -0500

    Second Commit

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,34 @@
-import UIKit
+/// Variables
+///
+///
 
-var greeting = "Hello, playground"
+let myName: String = "Vandad"
+var yourName: String = "Foo"
+
+var names = ["Vandad", "Foo"]
+names.append("Bar")
+names.append("Baz")
+
+let foo = "Foo"
+var foo2 = foo
+foo2 = "Foo 2"
+foo
+foo2
+
+let moreNames = [
+    "Foo",
+    "Bar",
+]
+var copyOfMoreNames = moreNames
+copyOfMoreNames.append("Baz")
+moreNames
+copyOfMoreNames
+
+let oldArray: [Int] = [1, 2, 3]
+var newArray: [Int] = []
+newArray.append(contentsOf: oldArray)
+newArray.append(4)
+newArray
+oldArray
+
+
