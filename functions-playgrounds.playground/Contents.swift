f510dc364e82681a74e32b5971b947e8deec76f6
Andres Prada <prada1295@gmail.com>
2025-05-29 09:23:34 -0500

    more functions

--- functions-playgrounds.playground/Contents.swift
+++ functions-playgrounds.playground/Contents.swift
@@ -1,3 +1,43 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+func noArgumentsAndNoReturbValue() {
+    "I don't know what I'm doing"
+}
+noArgumentsAndNoReturbValue()
+
+func plusTwo(value: Int) {
+    let newValue = value + 2
+}
+
+
+plusTwo(value: 10)
+
+
+func newPlusTwo(value: Int) -> Int {
+    return value + 2
+}
+
+newPlusTwo(value: 10)
+
+func customAdd(
+    a: Int,
+    b: Int
+) -> Int {
+    a + b
+}
+let customAdded = customAdd(
+    a: 10,
+    b: 20
+)
+
+func customMinus(
+    _ lhs: Int,
+    _ rhs: Int
+) -> Int {
+    lhs - rhs
+}
+
+let custommSubstracted = customMinus(
+    10,
+    20
+)
